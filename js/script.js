b8eff52e8083e25297e4c274a31cb8a29a12b34f
aqibtas <153101958+aqibtas@users.noreply.github.com>
2024-01-10 18:12:56 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.carousele').slick({
+        // Slick options go here
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: false,
+        autoplay: true
+    });
+});
